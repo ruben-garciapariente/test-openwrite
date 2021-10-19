c103383d4b1bd85270070fbab19daa72ba29f09a
Rubén García Pariente <ruben.garciapariente@gruposantander.es>
2021-10-19 15:01:14 +0200

    Case 1: do not work adddependency

---
+++ src/main/java/com/test/project/web/HelloController.java
@@ -0,0 +1,16 @@
+package com.test.project.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String sayHello() {
+		return "Hello world!";
+	}
+
+}
